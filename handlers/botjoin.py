0ab6981be1eabfb87c89df45a737ee007e766a21
ivanche7 <98470817+ivanche7@users.noreply.github.com>
2022-06-05 10:41:59 +0300

    Update botjoin.py

--- handlers/botjoin.py
+++ handlers/botjoin.py
@@ -3,8 +3,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 from helpers.decorators import errors, authorized_users_only
+from config import SUDO_USERS 
+import asyncio
 
-@Client.on_message(filters.group & filters.command(["katil"]))
+@Client.on_message(filters.group & filters.command(["katil", "asistan"]))
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -13,44 +15,77 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>**𝐁𝐞𝐧𝐢 𝐎𝐧𝐜𝐞 𝐘𝐨𝐧𝐞𝐭𝐢𝐜𝐢 𝐘𝐚𝐩𝐦𝐚𝐥𝐢𝐬𝐢𝐧 .**</b>",
+            "<b>Beni Önce Yönetici Yapmalısın</b>",
         )
         return
 
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "Sesmusic Asistan"
+        user.first_name =  "AsistanUserbot"
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id,"**𝐒𝐞𝐧 𝐠𝐞𝐥 𝐝𝐞𝐫𝐬𝐢𝐧 𝐝𝐞 𝐁𝐞𝐧 𝐠𝐞𝐥𝐦𝐞𝐳 𝐌𝐢𝐲𝐢𝐦 !**")
+        await USER.send_message(message.chat.id,"Senin İsteğin Üzerine Geldim")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>**𝐀𝐬𝐢𝐬𝐭𝐚𝐧 𝐆𝐫𝐮𝐛𝐚 𝐊𝐚𝐭𝐢𝐥𝐝𝐢 !**</b>",
+            "<b>Asistan zaten bu grupta 🙄</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🔵 𝐇𝐀𝐓𝐀 🔵\n **𝐊𝐮𝐥𝐥𝐚𝐧𝐢𝐜𝐢 {user.first_name} 𝐘𝐨𝐠𝐮𝐧 𝐊𝐮𝐥𝐥𝐚𝐧𝐢𝐦𝐥𝐚𝐫 𝐧𝐞𝐝𝐞𝐧𝐢𝐲𝐥𝐞 𝐆𝐫𝐮𝐛𝐚 𝐊𝐚𝐭𝐢𝐥𝐚𝐦𝐚𝐝𝐢 ! 𝐀𝐬𝐢𝐬𝐭𝐚𝐧𝐢𝐧 𝐲𝐚𝐬𝐚𝐤𝐥𝐢 𝐨𝐥𝐮𝐩 𝐨𝐥𝐦𝐚𝐝𝐢𝐠𝐢𝐧𝐝𝐚𝐧 𝐞𝐦𝐢𝐧 𝐨𝐥𝐮𝐧.**"
-            "\n\n **𝐘𝐚𝐝𝐚 𝐀𝐬𝐢𝐬𝐭𝐚𝐧 𝐇𝐞𝐬𝐚𝐛𝐢𝐧𝐢 𝐊𝐞𝐧𝐝𝐢𝐧 𝐄𝐤𝐥𝐞** </b>",
+            f"<b>🔵 Zaman Aşımı Hatası 🔵\n User {user.first_name} userbot için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
+            "\n\n Yada Asistan Hesabını Gruba Kendin Ekle </b>",
         )
         return
     await message.reply_text(
-            "<b>**𝐀𝐬𝐢𝐬𝐭𝐚𝐧 𝐙𝐚𝐭𝐞𝐧 𝐆𝐫𝐮𝐛𝐭𝐚 𝐕𝐚𝐫 !**</b>",
+            "<b>Asistan zaten bu grupta 🙄</b>",
         )
     
-@USER.on_message(filters.group & filters.command(["ayril"]))
+@USER.on_message(filters.group & filters.command(["ayril", "asistanby"]))
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>**𝐀𝐬𝐢𝐬𝐭𝐚𝐧 𝐆𝐫𝐮𝐛𝐭𝐚𝐧 𝐀𝐲𝐫𝐢𝐥𝐚𝐦𝐚𝐝𝐢 !**"
-            "\n\n**𝐘𝐚 𝐝𝐚 𝐊𝐞𝐧𝐝𝐢𝐧 𝐂𝐢𝐤𝐚𝐫𝐚𝐛𝐢𝐥𝐢𝐫𝐬𝐢𝐧**</b>",
+            f"<b>Kullanıcı grubunuzdan ayrılamadı!."
+            "\n\nYada Kendin Çıkarabilirsin</b>",
         )
         return
  
- 
- 
+@USER.on_message(filters.group & filters.command(["userbotleave"]))
+@authorized_users_only
+async def rem(USER, message):
+    try:
+        await USER.leave_chat(message.chat.id)
+    except:
+        await message.reply_text(
+            f"<b>Kullanıcı grubunuzdan ayrılamadı! Sel bekliyor olabilir."
+            "\n\nVeya beni Grubunuzdan manuel olarak tekmeleyin</b>",
+        )
+        return
+
+
+@Client.on_message(filters.command(["maho"]))
+async def bye(client, message):
+    if message.from_user.id in SUDO_USERS:
+        left = 0
+        failed = 0
+        lol = await message.reply("Tüm Gruplardan ayrılıyorum...")
+        async for dialog in USER.iter_dialogs():
+            try:
+                await USER.leave_chat(dialog.chat.id)
+                left = left + 1
+                await lol.edit(
+                    f"Ayrılıyorum... Left: {left} chats. Failed: {failed} chats."
+                )
+            except:
+                failed = failed + 1
+                await lol.edit(
+                    f"Ayrılıyorum... Left: {left} chats. Failed: {failed} chats."
+                )
+            await asyncio.sleep(0.7)
+        await client.send_message(
+            message.chat.id, f"Left {left} chats. Failed {failed} chats."
+        )
